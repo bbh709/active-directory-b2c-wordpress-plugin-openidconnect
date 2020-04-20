0045e4b9771fe9740e44e4b71729e9cb4a3d4f64
Hugo Esperanca <hugo.esperanca@collaboris.co.uk>
2020-04-20 12:22:54 +0100

    Updated to latest version of the phpseclib

--- autoload.php
+++ autoload.php
@@ -6,19 +6,24 @@
  *  @param $class
  */
 function autoload($class) {
+	
+	if (false !== strpos($class,'phpseclib')) {
+		$test = 1;	
+	}
+		
 	// only try to autoload AD B2C classes or their vendor dependencies
-	if ( 0 !== strpos( $class, 'B2C' ) && 0 !== strpos( $class, 'Crypt' ) && 0 !== strpos ( $class, 'Math' ) ) {
+	if ( 0 !== strpos( $class, 'B2C' ) && 0 !== strpos( $class, 'phpseclib' ) ) {
 		return;
 	}
 
-	if (strpos($class, 'Crypt_') === false && strpos($class, 'Math_') === false) {
+	if ( strpos($class, 'phpseclib\Crypt') === false && strpos($class, 'phpseclib\Math') === false) {
 		$class_filename = 'class-' . strtolower(str_replace('_', '-', $class)) . '.php';
-	} else if (strpos($class, 'Math_') === false) {
-		$class_filename = 'phpseclib/Crypt/' . str_replace('Crypt_', '', $class) . '.php';
+	} else if (strpos($class, 'phpseclib\Math') === false ) {
+		$class_filename = 'phpseclib/Crypt/' . str_replace('phpseclib\Crypt\\', '', $class) . '.php';
 	} else {
-		$class_filename = 'phpseclib/Math/' . str_replace('Math_', '', $class) . '.php';
+		$class_filename = 'phpseclib/Math/' . str_replace('phpseclib\Math\\', '', $class) . '.php';
 	}
-
+	
 	$plugin_directory = plugin_dir_path( __FILE__ );
 
 	if ( file_exists( $plugin_directory.$class_filename ) ) {
